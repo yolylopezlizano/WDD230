8dbb1e6222746b0ae4768412e1f1314094a214e8
yolylizano <yolylizano@gmail.com>
2024-05-03 00:54:21 -0500

    learning activity week02

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,14 @@ document.getElementById("currentYear").textContent = currentYear;
 const lastModifiedElement = document.getElementById("lastModified");
 const lastModified = new Date(document.lastModified);
 lastModifiedElement.textContent = `Last modified: ${lastModified.toLocaleString()}`;
+
+function toggleMenu() {
+    var mobileMenu = document.getElementById("mobileMenu");
+    if (mobileMenu.style.display === "block") {
+        mobileMenu.style.display = "none";
+    } else {
+        mobileMenu.style.display = "block";
+    }
+}
+  
+
