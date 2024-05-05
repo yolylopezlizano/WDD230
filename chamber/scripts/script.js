fc08e743028e97a0ea58348a9069f4f11e3a40c4
yolylizano <yolylizano@gmail.com>
2024-05-04 21:57:44 -0500

    Changes in chamber page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -5,3 +5,25 @@ document.addEventListener("DOMContentLoaded", function() {
     var lastModified = new Date(document.lastModified);
     lastModifiedSpan.textContent = lastModified.toLocaleString();
 });
+
+const apiKey = '589eb38416d3410ba6d212507240405';
+
+// Hace una solicitud a la API de clima
+fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Tulsa&aqi=no`)
+    .then(response => response.json())
+// Dentro de tu código JavaScript
+.then(data => {
+    // Extrae la información relevante del clima
+    const temperature = data.current.temp_f;
+    const weatherDescription = data.current.condition.text;
+    const weatherIconUrl = data.current.condition.icon; // Suponiendo que la API proporciona un enlace directo al icono del clima
+
+    // Actualiza el contenido de la sección de clima en tu página web
+    const temperatureElement = document.querySelector('.temperature');
+    const descriptionElement = document.querySelector('.description');
+    const weatherIconElement = document.querySelector('.weather-icon');
+    
+    temperatureElement.textContent = temperature;
+    descriptionElement.textContent = weatherDescription;
+    weatherIconElement.src = weatherIconUrl; // Establece el enlace directo al icono del clima como la fuente de la imagen
+})
