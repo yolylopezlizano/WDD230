1ba955ae6ee9b4e5261ecea50b3a0e808fcc4238
yolylizano <yolylizano@gmail.com>
2024-05-11 01:09:26 -0500

    error file and corrections

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,7 +5,7 @@ const lastModified = new Date(document.lastModified);
 const formattedLastModified = lastModified.toLocaleString();
 
 document.getElementById("lastModifiedFooter").textContent = formattedLastModified;
-document.getElementById("lastModifiedSidebar").textContent = formattedLastModified;
+
 
 function toggleMenu() {
     var mobileMenu = document.getElementById("mobileMenu");
@@ -40,38 +40,9 @@ document.getElementById('visit-count').textContent = visitCount;
 
 const currentDate = new Date();
 
-const lastVisitString = localStorage.getItem('lastVisit');
-let lastVisit;
-if (lastVisitString) {
-    lastVisit = new Date(lastVisitString);
-} else {
-    lastVisit = currentDate;
-}
-
-const daysSinceLastVisit = dateDiffInDays(currentDate, lastVisit);
-
-let message;
-if (visitCount === 1) {
-    message = "Welcome! Let us know if you have any questions.";
-} else if (visitCount === 2) {
-    message = "Welcome back! Enjoy your visit!";
-} else {
-    const plural = daysSinceLastVisit === 1 ? "" : "s";
-    message = `You last visited ${daysSinceLastVisit} day${plural} ago.`;
-}
-
-const sidebarMessageContainer = document.getElementById('sidebar-message-container');
-
-const messageBox = document.createElement('div');
-messageBox.classList.add('sidebar-message-box');
-messageBox.textContent = message;
-
-sidebarMessageContainer.appendChild(messageBox);
-
-
 function initMap() {
 
-    geocodeAddress('Ruta Viva y Escalón Lumbisí, Esq. 170157, Cumbayá, Quito, Ecuador', function(coordinates) {
+    geocodeAddress('Ruta Viva, Esq. 170157, Cumbayá, Quito, Ecuador', function(coordinates) {
         var mapOptions = {
             center: coordinates, 
             zoom: 12 
@@ -82,7 +53,7 @@ function initMap() {
         var marker = new google.maps.Marker({
             position: coordinates,
             map: map,
-            title: 'Ruta Viva y Escalón Lumbisí, Esq. 170157, Cumbayá, Quito, Ecuador' 
+            title: 'Ruta Viva, Esq. 170157, Cumbayá, Quito, Ecuador' 
         });
     });
 }
@@ -99,45 +70,6 @@ function geocodeAddress(address, callback) {
     });
 }
 
-
-const apiKey = '589eb38416d3410ba6d212507240405';
-fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Tulsa&aqi=no`)
-    .then(response => response.json())
-.then(data => {
-    
-    const temperature = data.current.temp_f;
-    const weatherDescription = data.current.condition.text;
-    const weatherIconUrl = data.current.condition.icon; 
-
-    const temperatureElement = document.querySelector('.temperature');
-    const descriptionElement = document.querySelector('.description');
-    const weatherIconElement = document.querySelector('.weather-icon');
-    
-    temperatureElement.textContent = temperature;
-    descriptionElement.textContent = weatherDescription;
-    weatherIconElement.src = weatherIconUrl; 
-})
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const lazyImages = document.querySelectorAll(".lazy-image");
-
-    function fadeInImages() {
-        lazyImages.forEach(function(img) {
-            const rect = img.getBoundingClientRect();
-            const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-            const threshold = rect.height * 0.2; 
-            if (rect.top <= windowHeight && rect.bottom >= threshold) {
-                img.classList.remove("hidden");
-            } else {
-                img.classList.add("hidden");
-            }
-        });
-    }
-
-    fadeInImages();
-    window.addEventListener("scroll", fadeInImages);
-});
 
 
 
