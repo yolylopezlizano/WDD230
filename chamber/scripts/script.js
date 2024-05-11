a098020ff5522255566d906cf48d105ac412076a
yolylizano <yolylizano@gmail.com>
2024-05-11 03:08:23 -0500

    correction file discovery

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -37,4 +37,73 @@ function toggleMenu() {
     }
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth'
+    });
+    calendar.render();
+  });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const lazyImages = document.querySelectorAll(".lazy-image");
+
+    function fadeInImages() {
+        lazyImages.forEach(function(img) {
+            const rect = img.getBoundingClientRect();
+            const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+            const threshold = rect.height * 0.2; 
+            if (rect.top <= windowHeight && rect.bottom >= threshold) {
+                img.style.opacity = 1; // Cambia la opacidad gradualmente a 1
+            } else {
+                img.style.opacity = 0; // Oculta la imagen si no está visible
+            }
+        });
+    }
+
+    fadeInImages();
+    window.addEventListener("scroll", fadeInImages);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lastVisit = localStorage.getItem("lastVisit");
+    console.log("Last visit:", lastVisit); 
+    
+    if (lastVisit) {
+        lastVisit = parseInt(lastVisit); 
+        
+        var currentTime = Date.now(); 
+        console.log("Current time:", currentTime); 
+        
+        var differenceInMilliseconds = currentTime - lastVisit;
+        var differenceInDays = Math.floor(differenceInMilliseconds); 
+        console.log("Difference in days:", differenceInDays); 
+
+        var sidebarMessage = "";
+        if (differenceInDays === 0) {
+            sidebarMessage = "Back so soon! Awesome!";
+        } else if (differenceInDays === 1) {
+            sidebarMessage = "You last visited 1 day ago.";
+        } else {
+            sidebarMessage = "You last visited " + differenceInDays + " days ago.";
+        }
+    } else {
+
+        sidebarMessage = "Welcome! Let us know if you have any questions.";
+    }
+    
+    var sidebarContent = document.querySelector(".sidebar-content");
+    sidebarContent.textContent = sidebarMessage;
+
+    localStorage.setItem("lastVisit", currentTime.toString()); 
+});
+
+
+
+
+
+
+      
+    
+    
+   
