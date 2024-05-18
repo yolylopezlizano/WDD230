04a8ef1e8dadd03d6f297657c8c713546652203d
yolylizano <yolylizano@gmail.com>
2024-05-17 19:08:33 -0500

    changes web lab

--- scripts/form.js
+++ scripts/form.js
@@ -1,7 +1,3 @@
-function updateRatingValue(value) {
-    document.getElementById('ratingValue').innerText = value;
-}
-
 function validatePasswords() {
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
@@ -14,4 +10,20 @@ function validatePasswords() {
         return false;
     }
     return true;
+}
+
+function validateEmail() {
+    var email = document.getElementById('email').value;
+    var emailPattern = /[a-zA-Z0-9._%+-]+@byui\.edu$/;
+
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address ending with @byui.edu");
+        return false; 
+    }
+
+    return true; 
+}
+
+function updateRatingValue(value) {
+    document.getElementById('ratingValue').innerText = value;
 }
